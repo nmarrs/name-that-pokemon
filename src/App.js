8a255a9558e4f74624b7472cf9889faf3ea6980a
Nathan Marrs <nathan.marrs@fluentworlds.com>
2021-07-26 22:00:31 -0700

    Set up emotion for css in js styling solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/react'
+
 import logo from './logo.svg'
-import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContainer>
+      <AppHeader>
+        <AppLogo src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <AppLink
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </AppContainer>
   )
 }
 
 export default App
+
+const AppContainer = styled.div`
+  text-align: center;
+`
+
+const AppLogoSpin = keyframes`
+from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: '${AppLogoSpin}' infinite 20s linear;
+  }
+`
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+
+const AppLink = styled.a`
+  color: #61dafb;
+`
